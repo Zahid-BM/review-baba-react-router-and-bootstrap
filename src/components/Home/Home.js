6da66994624054530358796db864979c54fa8240
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-04-05 16:15:28 +0600

    Alhamdulillah home page all done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import useReviews from '../../hooks/useReviews';
+import Reviews from '../Reviews/Reviews';
 
 const Home = () => {
+    const [reviews, setReviews] = useReviews();
     return (
         <div className='container'>
             <div className="row pt-5 my-5">
@@ -12,10 +16,18 @@ const Home = () => {
                     <img className='w-100 h-100 rounded' src={'https://img.freepik.com/free-psd/abstract-background-with-opened-book_23-2149085907.jpg?w=826&t=st=1649146958~exp=1649147558~hmac=0b4c9c0c11e9ddb8920a4cacd38d8525c74aac557ee5b068871f57000e2ca315'} alt="" />
                 </div>
             </div>
-            <div className="col">
-
+            <div className="row row-col-md-3 mb-5">
+                <h1 className='fw-bolder display-4 my-5 text-info border-bottom border-5 '>Public Reviews about the Book</h1>
+                {
+                    reviews.slice(0, 3).map(review => <Reviews key={review.id} review={review}></Reviews>)
+                }
             </div>
 
+            <Link to='/reviews'>
+                <div className="d-grid gap-2 col-6 mx-auto my-5">
+                    <button className="btn btn-primary" type="button">See all reviews</button>
+                </div>
+            </Link>
         </div>
     );
 };
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Reviews.css'
 
-const Reviews = () => {
+const Reviews = ({ review }) => {
+    const { name, picture, rating, comment } = review;
     return (
-        <div>
-            <h2>This is Review</h2>
+        <div className="card g-3 mx-2 my-5 " style={{ width: '22rem' }}>
+            <img src={picture} className="card-img-top reviewer-img" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">{name}</h5>
+                <h5 className="card-subtitle mb-2 text-warning fw-bolder fs-4"> Rating :{rating}</h5>
+                <p className="card-text">{comment}</p>
+            </div>
+
         </div>
     );
 };

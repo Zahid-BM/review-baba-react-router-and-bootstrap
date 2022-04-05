ba3a8ef656c4d4386514e2092f7b509db2122ada
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-04-05 23:30:45 +0600

    Alhamdulillah about responsive fixed

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className='border border-4 border-info rounded p-5 w-50 mx-auto my-5'>
-            <h2 className='text-secondary display-1'>No Specific Guidelines.</h2>
-            <h3 className='text-success'>That's why kept empty.</h3>
+        <div className="container">
+            <div className='border border-4 border-info rounded p-5 w-50 mx-auto my-5'>
+                <h2 className='text-secondary display-sm-1'>No Specific Guidelines</h2>
+                <h3 className='text-success display-sm-3'>That's why kept empty.</h3>
+            </div>
         </div>
     );
 };
